5f4386216dd28a596ffc8c6117680993406d2de4
YauheniKorsakas <yauheni.korsakas@gmail.com>
2022-04-10 22:15:42 +0300

    Adaptiveness refactoring

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -4,8 +4,8 @@ import { AiOutlineCheck } from 'react-icons/ai';
 
 const Description = () => {
   const listItems = [
-    <>Monitor your health. No one will take care of you except yourself. I do it because I want <span>to</span></>,
-    <>Be able to get better every day <span>because</span></>,
+    <>Monitor your health. No one will take care of you except yourself. I do it because I want <span>to...</span></>,
+    <>Be able to get better every day <span>because...</span></>,
     <>I want to know how far I can go, striving for the ideal</>
   ];
   const postulates = listItems.map((item) => <li key={item}><AiOutlineCheck /> {item}</li>);
@@ -16,7 +16,7 @@ const Description = () => {
         Greetings. My name is Zhenya and I am a full stack web developer from Belarus. Being born in 1996,
         I have already managed to know different aspects of life in general and development, but it is still not enough to say
         that I am wise and enlightened. Every day I try to become better and I believe that the meaning of life for each person
-        is individual. And the life postulates that I adhere to are as <span>follows</span>
+        is individual. And the life postulates that I adhere to are as <span>follows:</span>
       </p>
       <ul>
         {postulates}
